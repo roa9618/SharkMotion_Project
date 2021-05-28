4bf2a6ade80d1dd9795924b44fd3f8427006122a
Woohyeon Roh <ykjrco@naver.com>
2021-05-29 02:00:47 +0900

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,24 +1,27 @@
 #include <Servo.h>
 
-Servo servo;
+Servo servo1, servo2;
 
 void mouth(int angle1, int angle2, int seconds1, int seconds2)
 {
   for (int i = angle1; i <= angle2; i++)
   {
-    servo.write(i);
+    servo1.write(i);
+    servo2.write(i);
     delay(seconds1);
   }
   for (int i = angle2; i >= angle1; i--)
   {
-    servo.write(i);
+    servo1.write(i);
+    servo2.write(i);
     delay(seconds2);
   }
 }
 
 void setup()
 {
-  servo.attach(10);
+  servo1.attach(10);
+  servo2.attach(5);
 }
 
 void loop()
