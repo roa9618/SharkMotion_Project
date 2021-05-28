ff84930d9ac375d6a1fae802e99f54cc96e4ea59
Woohyeon Roh <ykjrco@naver.com>
2021-05-29 01:24:33 +0900

    Update irremote_test.ino : apply LED on/off func

--- irremote_test.ino
+++ irremote_test.ino
@@ -1,14 +1,19 @@
 #include <IRremote.h>
 
-IRrecv irrecv(11);
+int RECV_PIN = 13;
+int LED_PIN1 = 5;
+int LED_PIN2 = 6;
+int i = 0;
+
+IRrecv irrecv(RECV_PIN);
 decode_results results;
 
 void setup()
 {
   Serial.begin(9600);
   irrecv.enableIRIn();
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
 }
 
 void loop()
@@ -23,6 +28,28 @@ void loop()
     case 0xFF18E7 : digitalWrite(LED_PIN1, LOW); break;
     case 0xFF7A85 : digitalWrite(LED_PIN2, HIGH); break;
     case 0xFF10EF : digitalWrite(LED_PIN2, LOW); break;
+    case 0xFF5AA5 :
+      while (i < 6)
+      {
+        if (i != 5)
+        {
+          digitalWrite(LED_PIN1, HIGH);
+          delay(100);
+          digitalWrite(LED_PIN2, LOW);
+          delay(100);
+          digitalWrite(LED_PIN1, LOW);
+          delay(100);
+          digitalWrite(LED_PIN2, HIGH);
+          delay(100);
+          digitalWrite(LED_PIN1, LOW);
+          delay(100);
+          digitalWrite(LED_PIN2, LOW);
+          delay(100);
+          i++;
+        }
+        else
+          break;
+      }
   }
   irrecv.resume();
 }
