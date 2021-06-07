f35b9e68b390d33c5e84d468dc06a32fc93865a4
Woohyeon Roh <ykjrco@naver.com>
2021-06-08 01:58:14 +0900

    Update tail.ino

--- tail.ino
+++ tail.ino
@@ -1,6 +1,23 @@
 #include <Servo.h>
+#include <IRremote.h>
 
-Servo servo3;
+IRrecv irrecv(13);
+decode_results results;
+Servo servo1, servo3;
+
+void mouth(int angle1, int angle2, int seconds1, int seconds2)
+{
+  for (int i = angle1; i <= angle2; i++)
+  {
+    servo1.write(i);
+    delay(seconds1);
+  }
+  for (int i = angle2; i >= angle1; i--)
+  {
+    servo1.write(i);
+    delay(seconds2);
+  }
+}
 
 void tail(int angle1, int angle2, int seconds)
 {
@@ -19,10 +36,36 @@ void tail(int angle1, int angle2, int seconds)
 
 void setup()
 {
-  servo3.attach(8);
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  servo1.attach(10);
+  servo3.attach(6);
 }
 
 void loop()
 {
-  tail(40, 135, 10);
+  int i = 0;
+  int j = 0;
+  if (irrecv.decode(&results) == true)
+  {
+    switch (results.value)
+    {
+      case 0xFF30CF :
+        while (i <= 5)
+        {
+          mouth(45, 150, 3, 6);
+          delay(1500);
+          i++;
+        }
+        break;
+      case 0xFF18E7 :
+        while (j <= 5)
+        {
+          tail(45, 135, 10);
+          j++;
+        }
+        break;
+    }
+  }
+  irrecv.resume();
 }
